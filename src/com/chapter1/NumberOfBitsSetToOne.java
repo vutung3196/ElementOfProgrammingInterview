f88e6b4faa34cbb95f9876a5f1c84011a4d68021
Tung Vu <vutung3196@outlook.com>
2021-03-21 15:43:41 +0700

    Add binary problems

--- src/com/chapter1/NumberOfBitsSetToOne.java
+++ src/com/chapter1/NumberOfBitsSetToOne.java
@@ -3,6 +3,8 @@ package com.chapter1;
 public class NumberOfBitsSetToOne {
 
     public static void main(String[] args) {
+        // Writing a program to count the number of bits that are set to 1 in an integer
+        // Time complexity: O(1), where n is the number of bits in the integer word
         System.out.println(calculate(1234));
         System.out.println(calculate(1235));
         System.out.println(calculate(1236));
@@ -14,7 +16,7 @@ public class NumberOfBitsSetToOne {
         short result = 0;
         while (number != 0) {
             result += number & 1;
-            // right shift
+            // right shift the number
             number >>>= 1;
         }
         return result;
---
+++ src/com/chapter10/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package com.chapter10;
+
+public class BinaryTreeNode<T> {
+    public T data;
+    public BinaryTreeNode<T> left, right;
+
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
+
